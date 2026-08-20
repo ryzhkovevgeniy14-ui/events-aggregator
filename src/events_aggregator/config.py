6f6594c2b4ef07bd0792f955d808bdf280feb7ff
Red-14 <red-14@gmail.com>
2026-08-21 01:19:09 +0300

    feat: add events provider configuration

--- src/events_aggregator/config.py
+++ src/events_aggregator/config.py
@@ -8,9 +8,10 @@ class Settings(BaseSettings):
     postgres_port: int
     postgres_username: str
     postgres_password: str
+    events_provider_base_url: str
+    events_provider_api_key: str
 
     model_config = SettingsConfigDict(
-        env_file=".env",
         extra="ignore",
     )
 
