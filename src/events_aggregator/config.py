f87422d0803995d38565509f97d9e92b019eaf11
Red-14 <red-14@gmail.com>
2026-08-20 15:11:16 +0300

    feat: add application settings

---
+++ src/events_aggregator/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    postgres_connection_string: str
+    postgres_database_name: str
+    postgres_host: str
+    postgres_port: int
+    postgres_username: str
+    postgres_password: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore",
+    )
+
+
+settings = Settings()
