bee3166852c71d9daea2ae11016e33a7e7982af2
Red-14 <red-14@gmail.com>
2026-08-20 15:11:46 +0300

    feat: add database session management

---
+++ src/events_aggregator/db/depends.py
@@ -0,0 +1,11 @@
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from events_aggregator.db.session import async_session_maker
+
+
+# Предоставляет асинхронную сессию SQLAlchemy для работы с базой данных PostgreSQL
+async def get_async_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
---
+++ src/events_aggregator/db/session.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from events_aggregator.config import settings
+
+database_url = settings.postgres_connection_string.replace(
+    "postgres://",
+    "postgresql+asyncpg://",
+)
+
+async_engine = create_async_engine(
+    database_url,
+    echo=True,
+)
+
+async_session_maker = async_sessionmaker(
+    async_engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
