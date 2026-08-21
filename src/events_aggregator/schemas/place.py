af7ab2f4a03fb23c3a4642dfd56e7e48413559a0
Red-14 <red-14@gmail.com>
2026-08-21 19:54:51 +0300

    feat: add place list schema

--- src/events_aggregator/schemas/place.py
+++ src/events_aggregator/schemas/place.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class PlaceResponse(BaseModel):
@@ -12,3 +12,13 @@ class PlaceResponse(BaseModel):
     seats_pattern: str
     changed_at: datetime
     created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+class PlaceListItem(BaseModel):
+    id: UUID
+    name: str
+    city: str
+    address: str
+
+    model_config = ConfigDict(from_attributes=True)
