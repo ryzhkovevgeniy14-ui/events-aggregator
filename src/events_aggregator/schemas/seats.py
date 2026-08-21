7f47b9b59584f41b35ea3a61ff62bc0f038b4390
Red-14 <red-14@gmail.com>
2026-08-21 23:50:01 +0300

    feat: add provider seats response schema

--- src/events_aggregator/schemas/seats.py
+++ src/events_aggregator/schemas/seats.py
@@ -3,6 +3,10 @@ from uuid import UUID
 from pydantic import BaseModel
 
 
+class ProviderSeatsResponse(BaseModel):
+    seats: list[str]
+
+
 class SeatsResponse(BaseModel):
     event_id: UUID
     available_seats: list[str]
