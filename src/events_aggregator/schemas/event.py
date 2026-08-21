9f9864e207a0a1266b094a2394b145f44835b83f
Red-14 <red-14@gmail.com>
2026-08-21 19:55:07 +0300

    feat: add event list schemas

--- src/events_aggregator/schemas/event.py
+++ src/events_aggregator/schemas/event.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 from datetime import datetime
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
-from events_aggregator.schemas.place import PlaceResponse
+from events_aggregator.schemas.place import PlaceListItem, PlaceResponse
 
 
 class EventResponse(BaseModel):
@@ -20,8 +20,29 @@ class EventResponse(BaseModel):
     created_at: datetime
     status_changed_at: datetime
 
+    model_config = ConfigDict(from_attributes=True)
+
 
 class EventsResponse(BaseModel):
     next: str | None
     previous: str | None
     results: list[EventResponse]
+
+
+class EventListItem(BaseModel):
+    id: UUID
+    name: str
+    place: PlaceListItem
+    event_time: datetime
+    registration_deadline: datetime
+    status: str
+    number_of_visitors: int
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class EventsListResponse(BaseModel):
+    count: int
+    next: str | None
+    previous: str | None
+    results: list[EventListItem]
