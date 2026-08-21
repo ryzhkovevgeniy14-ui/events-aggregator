c139c60b64d9e6cde9a019dea3389b18a9e79c94
Red-14 <red-14@gmail.com>
2026-08-22 00:55:08 +0300

    feat: add ticket registration request schema

--- src/events_aggregator/schemas/ticket.py
+++ src/events_aggregator/schemas/ticket.py
@@ -3,6 +3,14 @@ from uuid import UUID
 from pydantic import BaseModel
 
 
+class RegisterRequest(BaseModel):
+    event_id: UUID
+    first_name: str
+    last_name: str
+    email: str
+    seat: str
+
+
 class RegisterResponse(BaseModel):
     ticket_id: UUID
 
