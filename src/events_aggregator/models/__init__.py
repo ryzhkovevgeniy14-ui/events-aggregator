4248f05bb2845ee53694909cea0cbfdfdae33942
Red-14 <red-14@gmail.com>
2026-09-04 17:08:18 +0300

    feat: register outbox model

--- src/events_aggregator/models/__init__.py
+++ src/events_aggregator/models/__init__.py
@@ -1,10 +1,12 @@
 from events_aggregator.models.event import Event
+from events_aggregator.models.outbox import Outbox
 from events_aggregator.models.place import Place
 from events_aggregator.models.sync_state import SyncState
 from events_aggregator.models.ticket import Ticket
 
 __all__ = [
     "Event",
+    "Outbox",
     "Place",
     "SyncState",
     "Ticket",
