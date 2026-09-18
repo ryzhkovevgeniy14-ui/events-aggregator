2c18f90ad375a6ed52cf2190698fc251d3eec5da
Red-14 <red-14@gmail.com>
2026-09-18 23:56:44 +0300

    feat: add HTTP metrics

---
+++ src/events_aggregator/core/metrics.py
@@ -0,0 +1,14 @@
+from prometheus_client import Counter, Histogram
+
+http_requests_total = Counter(
+    "http_requests_total",
+    "Total number of HTTP requests",
+    ["method", "endpoint", "status"],
+)
+
+http_request_duration_seconds = Histogram(
+    "http_request_duration_seconds",
+    "HTTP request duration in seconds",
+    ["method", "endpoint"],
+    buckets=[0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0],
+)
