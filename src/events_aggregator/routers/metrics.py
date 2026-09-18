9adf761b4fa8d2b0373ebe529ba9cd43b4815954
Red-14 <red-14@gmail.com>
2026-09-18 23:09:25 +0300

    feat: add metrics endpoint

---
+++ src/events_aggregator/routers/metrics.py
@@ -0,0 +1,13 @@
+from prometheus_client import REGISTRY, generate_latest
+from fastapi import APIRouter
+from fastapi.responses import Response
+
+router = APIRouter()
+
+
+@router.get("/metrics")
+def metrics():
+    return Response(
+        content=generate_latest(REGISTRY),
+        media_type="text/plain",
+    )
