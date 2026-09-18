dc1be2e12231c6f5a79433fea9f35c79a5227b3b
Red-14 <red-14@gmail.com>
2026-09-19 01:38:01 +0300

    feat: add database metrics

--- src/events_aggregator/routers/metrics.py
+++ src/events_aggregator/routers/metrics.py
@@ -1,12 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.responses import Response
 from prometheus_client import REGISTRY, generate_latest
+
+from events_aggregator.core.metrics import (
+    events_total,
+    tickets_cancelled_total,
+    tickets_created_total,
+)
+from events_aggregator.dependencies import (
+    get_event_repository,
+    get_ticket_repository,
+)
+from events_aggregator.repositories.event import EventRepository
+from events_aggregator.repositories.ticket import TicketRepository
 
 router = APIRouter()
 
 
 @router.get("/metrics")
-def metrics():
+async def metrics(
+    events_repo: EventRepository = Depends(get_event_repository),  # noqa: B008
+    tickets_repo: TicketRepository = Depends(get_ticket_repository),  # noqa: B008
+):
+    events_total.set(await events_repo.count())
+    tickets_created_total.set(await tickets_repo.count())
+    tickets_cancelled_total.set(await tickets_repo.count_cancelled())
+
     return Response(
         content=generate_latest(REGISTRY),
         media_type="text/plain",
